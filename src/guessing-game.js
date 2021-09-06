5f3ffcce7222a1c00aefbe6d6999b0e631713eff
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-09-06 10:50:36 +0300

    task ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.minNmb = min;
+        this.maxNmb = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.minNmb + this.maxNmb) / 2);
+        return this.result;
     }
 
     lower() {
+        this.maxNmb = this.result;
 
     }
 
     greater() {
+        this.minNmb = this.result;
 
     }
 }
